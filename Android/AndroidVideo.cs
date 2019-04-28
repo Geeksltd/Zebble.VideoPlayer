7ee2baaaea92f3708d7088808699abb04a9c75b1
Paymon <paymon@geeks.ltd.uk>
2019-04-28 13:03:15 +0100

    no message

--- Android/AndroidVideo.cs
+++ Android/AndroidVideo.cs
@@ -1,182 +1,92 @@
 namespace Zebble
 {
-    using Android.Graphics;
-    using Android.Media;
     using Android.Runtime;
     using Android.Views;
     using Android.Widget;
     using System;
     using Zebble.Device;
-    using static Zebble.VideoPlayer;
 
-    class AndroidVideo : RelativeLayout, ISurfaceHolderCallback, MediaPlayer.IOnPreparedListener
+    class AndroidVideo : VideoView
     {
         VideoPlayer View;
 
-        SurfaceView VideoSurface;
-        MediaPlayer VideoPlayer;
-        Preparedhandler Prepared = new Preparedhandler();
-        bool IsSurfaceCreated;
-
-        public AndroidVideo(IntPtr handle, JniHandleOwnership transfer) : base(UIRuntime.CurrentActivity)
-        {
-        }
+        public AndroidVideo(IntPtr handle, JniHandleOwnership transfer) : base(UIRuntime.CurrentActivity) { }
 
         public AndroidVideo(VideoPlayer view) : base(UIRuntime.CurrentActivity)
         {
             View = view;
-            CreateSurfceView();
-            CreateVideoPlayer();
+            Completion += OnCompletion;
+            LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent);
 
-            View.Buffered.HandleOn(Thread.UI, () => VideoPlayer?.PrepareAsync());
-            View.PathChanged.HandleOn(Thread.UI, () => { if (View.AutoPlay) LoadVideo(); });
-            View.Started.HandleOn(Thread.UI, () => Prepared.Raise(VideoState.Play));
-            View.Paused.HandleOn(Thread.UI, () => Prepared.Raise(VideoState.Pause));
-            View.Resumed.HandleOn(Thread.UI, () => Prepared.Raise(VideoState.Play));
-            View.Stopped.HandleOn(Thread.UI, () => Prepared.Raise(VideoState.Stop));
-            View.SoughtBeginning.HandleOn(Thread.UI, () => Prepared.Raise(VideoState.SeekToBegining));
-            Prepared.Handle(HandleStateCommand);
+            view.Started.HandleOn(Thread.UI, () => SafeInvoke(Start));
+            view.Paused.HandleOn(Thread.UI, () => SafeInvoke(Pause));
+            view.Resumed.HandleOn(Thread.UI, () => SafeInvoke(Resume));
+            view.Stopped.HandleOn(Thread.UI, () => SafeInvoke(StopPlayback));
+            view.SoughtBeginning.HandleOn(Thread.UI, () => SafeInvoke(() => SeekTo(0)));
+            view.PathChanged.HandleOn(Thread.UI, () => SafeInvoke(SetSource));
+            SetSource();
         }
 
-        void CreateSurfceView()
+        void SafeInvoke(Action action)
         {
-            VideoSurface = new SurfaceView(UIRuntime.CurrentActivity);
-            VideoSurface.Holder.AddCallback(this);
-            VideoSurface.LayoutParameters = CreateLayout();
-            AddView(VideoSurface);
+            if (IsDead(out var view)) return;
+
+            try { action(); }
+            catch (Exception ex) { Log.Error(ex); }
         }
 
-        void CreateVideoPlayer()
+        void SetSource()
         {
-            VideoPlayer = new MediaPlayer();
-            VideoPlayer.SetOnPreparedListener(this);
-            VideoPlayer.Completion += OnCompletion;
-            VideoPlayer.VideoSizeChanged += OnVideoSizeChanged;
-        }
+            if (IsDead(out var view)) return;
 
-        LayoutParams CreateLayout()
-        {
-            var result = new LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent);
-            result.AddRule(LayoutRules.AlignParentTop);
-            result.AddRule(LayoutRules.AlignParentBottom);
-            result.AddRule(LayoutRules.AlignParentLeft);
-            result.AddRule(LayoutRules.AlignParentRight);
-            return result;
-        }
+            var source = view.Path;
+            if (source.LacksValue()) return;
 
-        public override ViewStates Visibility
-        {
-            get => base.Visibility;
-            set
+            if (IO.IsAbsolute(source)) source = "file://" + source;
+            else if (!source.IsUrl()) source = IO.AbsolutePath(source);
+            SetVideoURI(Android.Net.Uri.Parse(source));
+
+            view.WhenShown(() =>
             {
-                if (value == ViewStates.Visible)
-                    LayoutParameters = View.GetFrame();
-                else if (value == ViewStates.Invisible || value == ViewStates.Gone)
-                    LayoutParameters = new FrameLayout.LayoutParams(0, 0);
-            }
+                if (view.AutoPlay) Start();
+            });
         }
 
-        public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height) { }
-
-        public void SurfaceCreated(ISurfaceHolder holder)
+        async void OnCompletion(object e, EventArgs args)
         {
-            IsSurfaceCreated = holder.Surface?.IsValid == true;
-
-            VideoPlayer.SetDisplay(VideoSurface.Holder);
-
-            if (View.AutoPlay && IsSurfaceCreated)
-                LoadVideo();
+            if (IsDead(out var view)) return;
+            await view.FinishedPlaying.RaiseOn(Thread.Pool);
         }
 
-        public void SurfaceDestroyed(ISurfaceHolder holder)
+        protected override void OnSizeChanged(int w, int h, int oldw, int oldh)
         {
-            View.IsReady = false;
-            IsSurfaceCreated = false;
-            holder?.Surface?.Release();
-        }
+            base.OnSizeChanged(w, h, oldw, oldh);
+            if (IsDead(out var view)) return;
 
-        void HandleStateCommand(VideoState result)
-        {
-            switch (result)
-            {
-                case VideoState.Play:
-                    LoadVideo();
-                    break;
-                case VideoState.Pause:
-                    if (VideoPlayer?.IsPlaying == true) VideoPlayer.Pause();
-                    break;
-                case VideoState.Stop:
-                    VideoPlayer?.Stop();
-                    VideoPlayer?.Reset();
-                    break;
-                case VideoState.SeekToBegining:
-                    VideoPlayer?.Reset();
-                    break;
-                default: break;
-            }
-        }
-
-        public void OnPrepared(MediaPlayer mp)
-        {
-            VideoPlayer.SetVideoScalingMode(VideoScalingMode.ScaleToFitWithCropping);
-            View.IsReady = true;
-            if (View.AutoPlay) mp.Start();
-        }
-
-        void LoadVideo()
-        {
-            var path = View.Path;
-            if (path.LacksValue()) return;
-
-            if (!IsSurfaceCreated)
-            {
-                Log.Error("Surface is not created when LoadVideo is called!!");
-                return;
-            }
-
-            if (IO.IsAbsolute(path)) path = "file://" + path;
-            else if (!path.IsUrl()) path = IO.AbsolutePath(path);
-
-            try
-            {
-                VideoPlayer.Reset();
-                VideoPlayer.SetDataSource(path);
-                VideoPlayer.Looping = View.Loop;
-
-                if (!path.IsUrl() || View.AutoBuffer)
-                    VideoPlayer.PrepareAsync();
-            }
-            catch (Java.Lang.Exception ex)
-            {
-                Log.Error("This error is raised without seemingly affecting anything! " + ex.Message);
-            }
-        }
-
-        async void OnCompletion(object e, EventArgs args) => await View.FinishedPlaying.RaiseOn(Thread.Pool);
-
-        void OnVideoSizeChanged(object e, EventArgs args)
-        {
-            var view = View;
-            if (view == null || view.IsDisposing) return;
             if (view.VideoSize.Width == 0)
             {
-                view.VideoSize = new Size(VideoPlayer?.VideoWidth ?? 0, VideoPlayer?.VideoHeight ?? 0);
+                view.VideoSize = new Size(w, h);
                 view.LoadCompleted?.RaiseOn(Thread.Pool);
             }
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing && VideoPlayer != null)
+            if (disposing)
             {
-                VideoPlayer.Completion -= OnCompletion;
-                VideoPlayer.VideoSizeChanged -= OnVideoSizeChanged;
-                VideoPlayer.Release();
-                VideoPlayer.Dispose();
-                VideoPlayer = null;
+                Completion -= OnCompletion;
+                View = null;
             }
 
             base.Dispose(disposing);
+        }
+
+        [EscapeGCop("In this case an out parameter can improve the code.")]
+        public bool IsDead(out VideoPlayer result)
+        {
+            result = View;
+            if (result == null) return true;
+            return result.IsDisposing;
         }
     }
 }
