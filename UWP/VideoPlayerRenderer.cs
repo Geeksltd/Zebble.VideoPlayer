ad9ded6e0c4002ab7a51d4f3ca21923367beceeb
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2021-01-06 14:34:11 +0330

    Fix render async issue

--- UWP/VideoPlayerRenderer.cs
+++ UWP/VideoPlayerRenderer.cs
@@ -2,14 +2,16 @@ namespace Zebble
 {
     using System.Threading.Tasks;
     using Windows.UI.Xaml;
+    using Olive;
 
     class VideoPlayerRenderer : INativeRenderer
     {
         FrameworkElement Result;
 
-        public async Task<FrameworkElement> Render(Renderer renderer)
+        public Task<FrameworkElement> Render(Renderer renderer)
         {
-            return Result = await new UWPVideoViewer((VideoPlayer)renderer.View).Render();
+            return new UWPVideoViewer((VideoPlayer)renderer.View).Render()
+                .ContinueWith(x => Result = x.GetAlreadyCompletedResult());
         }
 
         public void Dispose() => Result = null;
